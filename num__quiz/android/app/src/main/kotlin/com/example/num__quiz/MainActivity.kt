55bc4122e8898f7d81efd0305e1221b963da58a2
seongwon030 <105052068+seongwon030@users.noreply.github.com>
2023-11-07 12:52:34 +0900

    num_quiz

---
+++ num__quiz/android/app/src/main/kotlin/com/example/num__quiz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.num__quiz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ num__quiz/lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+/// 홈 페이지
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String quiz = ""; // 보여줄 퀴즈
+
+  @override
+  void initState() {
+    super.initState();
+    print("시작");
+    getQuiz(); // 퀴즈 가져오기 호출
+  }
+
+  /// 퀴즈 가져오기
+  void getQuiz() async {
+    String trivia = await getNumberTrivia();
+    setState(() {
+      quiz = trivia;
+    });
+  }
+
+  /// Numbers API 호출하기
+  Future<String> getNumberTrivia() async {
+    // get 메소드로 URL 호출
+    Response result = await Dio().get('http://numbersapi.com/random/trivia');
+    String trivia = result.data; // 응답 결과 가져오기
+    print(trivia);
+    return trivia;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        color: Colors.pinkAccent,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // quiz
+            Expanded(
+              child: Center(
+                child: Text(
+                  quiz,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            // New Quiz 버튼
+            SizedBox(
+              height: 42,
+              child: ElevatedButton(
+                child: Text(
+                  "New Quiz",
+                  style: TextStyle(
+                    color: Colors.pinkAccent,
+                    fontSize: 24,
+                  ),
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.white),
+                ),
+                onPressed: () {
+                  // New Quiz 클릭시 퀴즈 가져오기
+                  getQuiz();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
